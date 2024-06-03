27f3cdb34041a05d5fd1bc1638a23a8c6404623e
leticiacamposv <leticiacamposv.bi@gmail.com>
2024-06-03 09:40:55 -0300

    Updated function for image, main script and comments.md

--- COMMENTS.md
+++ COMMENTS.md
@@ -1,3 +1,26 @@
-# Bibliotecas/ instaladas
+# 1. Bibliotecas instaladas
+## 1.1 Pré-processamento de textos
+- nltk
+- SpaCy
+- sklearn(TfidfVectorizer)
+- YAKE
+- hugging-face transformers 
+- translate
 
-EM CONSTRUÇÃO
+## 1.2 PDF
+- PyPDF2
+
+## 1.3 Audio/video
+- moviepy
+- SpeechRecognition
+
+## 1.4 Imagens
+- PIL
+- pytesseract (Requer instalação na máquina do modelo e scripts para diferentes linguagens)
+- sentencepiece
+
+## 1.5 Mecanismo de busca
+- elasticsearch 
+
+## 1.5 Outros
+- torch (requisito para hugging-face)
--- source/image_utils.py
+++ source/image_utils.py
@@ -1,7 +1,6 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image, ExifTags
 from translate import Translator
-from transformers import DonutProcessor, VisionEncoderDecoderModel
 import sentencepiece as spm
 import torch
 import pytesseract
@@ -36,19 +35,37 @@ def get_exif_data(image_path):
         return {ExifTags.TAGS.get(tag): value for tag, value in exif_data.items()}
     return {}
 
-def extract_text_from_image(image_path):
-    # Open the image
+def extract_text_from_image(image_path, custom_oem_psm_config=None):
+    """
+    Page segmentation modes: (PSM)
+    0    Orientation and script detection (OSD) only.
+    1    Automatic page segmentation with OSD.
+    2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+    3    Fully automatic page segmentation, but no OSD. (Default)
+    4    Assume a single column of text of variable sizes.
+    5    Assume a single uniform block of vertically aligned text.
+    6    Assume a single uniform block of text.
+    7    Treat the image as a single text line.
+    8    Treat the image as a single word.
+    9    Treat the image as a single word in a circle.
+    10    Treat the image as a single character.
+    11    Sparse text. Find as much text as possible in no particular order.
+    12    Sparse text with OSD.
+    13    Raw line. Treat the image as a single text line,
+        bypassing hacks that are Tesseract-specific.
+    
+    OCR Engine modes: (OEM)
+    0    Legacy engine only.
+    1    Neural nets LSTM engine only.
+    2    Legacy + LSTM engines.
+    3    Default, based on what is available.
+  """
     img = Image.open(image_path)
     
-    # Perform OCR using Tesseract
-    text = pytesseract.image_to_string(img, lang='por')  # Language code for Portuguese
-    
+    # OCR
+    if custom_oem_psm_config:
+        text = pytesseract.image_to_string(img, lang='por', config=custom_oem_psm_config)  # Language code for Portuguese
+    else:
+        text = pytesseract.image_to_string(img, lang='por')
     return text
 
-# Path to your image
-image_path = '../resources/Infografico-1.jpg'
-
-# Extract text from image
-extracted_text = extract_text_from_image(image_path)
-
-print("Extracted Text:", extracted_text)
